1b28c62c5e698f18bf7018b6b2510fd9d2fd35a1
Ronit Kadwane <ronit@jumpbyte.com>
2017-09-28 15:40:13 +0530

    GooglePlayService check added

--- android/src/main/java/com/jumpbyte/JBFirebasePhoneAuthModule.java
+++ android/src/main/java/com/jumpbyte/JBFirebasePhoneAuthModule.java
@@ -1,6 +1,8 @@
 
 package com.jumpbyte;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -10,6 +12,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
@@ -28,6 +32,7 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
 public class JBFirebasePhoneAuthModule extends ReactContextBaseJavaModule {
 
     private static final String TAG = "JBFirebasePhoneAuth";
@@ -43,16 +48,16 @@ public class JBFirebasePhoneAuthModule extends ReactContextBaseJavaModule {
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks phoneAuthProviderCallbacks;
     private String firebaseVerificationId;
 
-
     public JBFirebasePhoneAuthModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return REACT_CLASS;
-  }
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
     @Override
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
@@ -62,9 +67,8 @@ public class JBFirebasePhoneAuthModule extends ReactContextBaseJavaModule {
     }
 
     private void sendEvent(String eventName, Object params) {
-        getReactApplicationContext()
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(eventName, params);
+        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName,
+                params);
     }
 
     @ReactMethod
@@ -77,22 +81,21 @@ public class JBFirebasePhoneAuthModule extends ReactContextBaseJavaModule {
                 object.put("phoneNumber", firebaseUser.getPhoneNumber());
                 object.put("providerId", firebaseUser.getProviderId());
                 object.put("uid", firebaseUser.getUid());
-                firebaseUser.getToken(true)
-                        .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
-                            public void onComplete(@NonNull Task<GetTokenResult> task) {
-                                if (task.isSuccessful()) {
-                                    String idToken = task.getResult().getToken();
-                                    Log.i(TAG, "idToken >>> " + idToken);
-                                    try {
-                                        object.put("idToken", idToken);
-                                        promise.resolve(object.toString());
-                                    } catch (JSONException e) {
-                                        promise.reject("401", e.getMessage());
-                                        e.printStackTrace();
-                                    }
-                                }
+                firebaseUser.getToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                    public void onComplete(@NonNull Task<GetTokenResult> task) {
+                        if (task.isSuccessful()) {
+                            String idToken = task.getResult().getToken();
+                            Log.i(TAG, "idToken >>> " + idToken);
+                            try {
+                                object.put("idToken", idToken);
+                                promise.resolve(object.toString());
+                            } catch (JSONException e) {
+                                promise.reject("401", e.getMessage());
+                                e.printStackTrace();
                             }
-                        });
+                        }
+                    }
+                });
             } catch (JSONException e) {
                 promise.reject("401", e.getMessage());
                 e.printStackTrace();
@@ -105,29 +108,36 @@ public class JBFirebasePhoneAuthModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void verifyPhoneNumber(String phoneNumber, Promise promise) {
-        Log.i(TAG, "verifyPhoneNumber");
-        Log.i(TAG, "phoneNumber >> " + phoneNumber);
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        PhoneAuthProvider.getInstance(firebaseAuth).verifyPhoneNumber(
-                phoneNumber,
-                60,
-                TimeUnit.SECONDS,
-                getCurrentActivity(),
-                verificationCallback(promise));
+    public void updateGooglePlayServices() {
+        getCurrentActivity().startActivity(
+                new Intent("android.intent.action.VIEW", Uri.parse("market://details?id=com.google.android.gms")));
     }
 
     @ReactMethod
-    public void resendVerificationCode(String phoneNumber, Promise promise){
+    public void verifyPhoneNumber(String phoneNumber, Promise promise) {
+        final int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getCurrentActivity());
+        Log.i(TAG, GooglePlayServicesUtil.GOOGLE_PLAY_SERVICES_VERSION_CODE + "");
+        if (status != ConnectionResult.SUCCESS) {
+            Log.e(TAG, GooglePlayServicesUtil.getErrorString(status));
+            // ask user to update google play services.
+            promise.reject("401", GooglePlayServicesUtil.getErrorString(status));
+        } else {
+            Log.i("TAG", GooglePlayServicesUtil.getErrorString(status));
+            // google play services is updated.
+            Log.i(TAG, "verifyPhoneNumber");
+            Log.i(TAG, "phoneNumber >> " + phoneNumber);
+            firebaseAuth = FirebaseAuth.getInstance();
+
+            PhoneAuthProvider.getInstance(firebaseAuth).verifyPhoneNumber(phoneNumber, 60, TimeUnit.SECONDS,
+                    getCurrentActivity(), verificationCallback(promise));
+        }
+    }
+
+    @ReactMethod
+    public void resendVerificationCode(String phoneNumber, Promise promise) {
         Log.i(TAG, "resendVerificationCode");
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phoneNumber,
-                60,
-                TimeUnit.SECONDS,
-                getCurrentActivity(),
-                verificationCallback(promise),
-                phoneAuthResendToken);
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber, 60, TimeUnit.SECONDS, getCurrentActivity(),
+                verificationCallback(promise), phoneAuthResendToken);
     }
 
     @ReactMethod
@@ -138,8 +148,8 @@ public class JBFirebasePhoneAuthModule extends ReactContextBaseJavaModule {
         signIn(credential, promise);
     }
 
-    private  PhoneAuthProvider.OnVerificationStateChangedCallbacks verificationCallback(final Promise promise) {
-        phoneAuthProviderCallbacks =  new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks verificationCallback(final Promise promise) {
+        phoneAuthProviderCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential credential) {
                 Log.i(TAG, "onVerificationCompleted");
@@ -180,34 +190,33 @@ public class JBFirebasePhoneAuthModule extends ReactContextBaseJavaModule {
         return phoneAuthProviderCallbacks;
     }
 
-    private void signIn(PhoneAuthCredential credential, final Promise promise){
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Log.i(TAG, "signInWithCredential:success");
-                            firebaseUser = task.getResult().getUser();
-                            final JSONObject object = new JSONObject();
-                            try {
-                                object.put("phoneNumber", firebaseUser.getPhoneNumber());
-                                object.put("providerId", firebaseUser.getProviderId());
-                                object.put("uid", firebaseUser.getUid());
-                                promise.resolve(object.toString());
-                            } catch (JSONException e) {
-                                promise.reject("203", e.getMessage());
-                                e.printStackTrace();
-                            }
-                        } else {
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                Log.w(TAG, "Invalid code.");
-                                promise.reject("201", "FirebaseAuthInvalidCredentialsException : Invalid code");
-                            }
-                            promise.reject("202", "signInWithCredential:failure " + task.getException());
-                        }
+    private void signIn(PhoneAuthCredential credential, final Promise promise) {
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.i(TAG, "signInWithCredential:success");
+                    firebaseUser = task.getResult().getUser();
+                    final JSONObject object = new JSONObject();
+                    try {
+                        object.put("phoneNumber", firebaseUser.getPhoneNumber());
+                        object.put("providerId", firebaseUser.getProviderId());
+                        object.put("uid", firebaseUser.getUid());
+                        promise.resolve(object.toString());
+                    } catch (JSONException e) {
+                        promise.reject("203", e.getMessage());
+                        e.printStackTrace();
                     }
-                });
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.getException());
+                    if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                        Log.w(TAG, "Invalid code.");
+                        promise.reject("201", "FirebaseAuthInvalidCredentialsException : Invalid code");
+                    }
+                    promise.reject("202", "signInWithCredential:failure " + task.getException());
+                }
+            }
+        });
     }
 
     @ReactMethod
